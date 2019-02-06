ee1ecfd831280964c8fe348d3e9c23d2c327a956
boryana-ilieva <boryana.a.ilieva@gmail.com>
2019-02-06 18:01:18 +0100

    Added notes for planned descriptive analysis in notebook.

--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import pandas as pd
 from soepy.python.simulate.simulate_python import simulate
 
 # Generate simulated dataset
-data_frame = simulate('toy_model_init_file.yml') 
+data_frame = simulate('toy_model_init_file_1000.yml') 
 
 # Save data frame to csv file
-data_frame.to_csv('toy_model_sim_test.csv', sep = '\t')
+data_frame.to_csv('toy_model_sim_test_1000.csv', sep = '\t')
