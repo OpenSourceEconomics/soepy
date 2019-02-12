961262cceee971df8af1f06daacf126f33fc144b
boryana-ilieva <boryana.a.ilieva@gmail.com>
2019-02-12 15:04:29 +0100

    Finished simulation example and descriptive statistics.

--- simulate.py
+++ simulate.py
@@ -5,4 +5,4 @@ from soepy.python.simulate.simulate_python import simulate
 data_frame = simulate('toy_model_init_file_1000.yml') 
 
 # Save data frame to csv file
-data_frame.to_csv('toy_model_sim_test_1000.csv', sep = '\t')
+data_frame.to_csv('toy_model_sim_benchmark.csv', sep = '\t')
