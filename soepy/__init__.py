1946c7090999be934685639445a1b74cf84fd40a
Sebastian Becker <s3sebeck@uni-bonn.de>
2019-03-14 14:40:31 +0100

    first edits for solving the pathlib issue

---
+++ soepy/__init__.py
@@ -0,0 +1,3 @@
+from soepy.python.simulate.simulate_python import simulate
+from soepy.python.soepy_config import PACKAGE_DIR
+import soepy.python.soepy_config
--- soepy/python/soepy_config.py
+++ soepy/python/soepy_config.py
@@ -1,6 +1,6 @@
 """This module provides some configuration for the package."""
 import sys
-import os
+import soepy
 from pathlib import Path
 
 import numpy as np
@@ -10,6 +10,6 @@ np.testing.assert_equal(sys.version_info[0], 3)
 np.testing.assert_equal(sys.version_info[1] >= 5, True)
 
 # We rely on relative paths throughout the package.
-ROOT_DIR = Path.cwd()
+ROOT_DIR = Path(soepy.__path__[0])
 PACKAGE_DIR = ROOT_DIR.parents[0]
-TEST_RESOURCES_DIR = PACKAGE_DIR / "test" / "resources"
+TEST_RESOURCES_DIR = PACKAGE_DIR / "soepy" / "test" / "resources"
