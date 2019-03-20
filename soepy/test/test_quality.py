8007e17f369d50c20435103aaa639fb21d284cca
peisenha <eisenhauer@policy-lab.org>
2019-03-20 13:35:56 +0100

    fixed security issue

--- soepy/test/test_quality.py
+++ soepy/test/test_quality.py
@@ -8,4 +8,4 @@ from soepy.python.soepy_config import PACKAGE_DIR
 
 def test1():
     """This test runs flake8 to ensure the code quality."""
-    np.testing.assert_equal(subprocess.call(["flake8"], cwd=PACKAGE_DIR), 0)
+    np.testing.assert_equal(subprocess.call(["flake8"], shell=True, cwd=PACKAGE_DIR), 0)
