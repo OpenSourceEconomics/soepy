2bc93b336a66611e66aa8915a7c7a996d68da0ef
boryana-ilieva <boryana.a.ilieva@gmail.com>
2019-03-27 16:16:40 +0100

    Changed tutorial simulate.py and init file to new interface structure.

--- doc/tutorial/simulate.py
+++ doc/tutorial/simulate.py
@@ -1,28 +1,8 @@
-import yaml
-
-from pathlib import Path
-
 from soepy.python.simulate.simulate_python import simulate
 from soepy.test.auxiliary import cleanup
-from soepy.python.soepy_config import TEST_RESOURCES_DIR
-
-pathlist = Path(TEST_RESOURCES_DIR).glob("**/*.yml")
-files = [x for x in pathlist if x.is_file()]
-
-file_list = []
-for file in files:
-    file_list.append(str(file))
-
-file_list.sort()
-print(file_list[0])
-
-with open(file_list[0]) as y:
-    init_dict = yaml.load(y)
-
-print(init_dict["PARAMETERS"])
 
 # Generate simulated dataset
-data_frame = simulate(file_list[0])
+data_frame = simulate("toy_model_init_file_1000.yml")
 
 # Save data frame to csv file
 data_frame.to_csv("test.soepy.csv", sep="\t")
