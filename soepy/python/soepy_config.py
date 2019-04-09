bf329439b515d479cd9674299d8bdd0ba437f2e0
boryana-ilieva <boryana.a.ilieva@gmail.com>
2019-04-09 17:45:43 +0200

    Completed minor changes requested.

--- soepy/python/soepy_config.py
+++ soepy/python/soepy_config.py
@@ -4,12 +4,10 @@ from pathlib import Path
 
 import numpy as np
 
-import soepy
-
 # We only support modern Python.
 np.testing.assert_equal(sys.version_info[:2] >= (3, 6), True)
 
 # We rely on relative paths throughout the package.
-ROOT_DIR = Path(soepy.__path__[0])
+ROOT_DIR = Path(__file__).parent.parent.absolute()
 PACKAGE_DIR = ROOT_DIR.parents[0]
 TEST_RESOURCES_DIR = PACKAGE_DIR / "soepy" / "test" / "resources"
