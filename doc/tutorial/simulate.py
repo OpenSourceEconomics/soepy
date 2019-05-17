54db8bb70b689a9dd4afcd7dea4b789859fdb94f
boryana-ilieva <boryana.a.ilieva@gmail.com>
2019-05-17 14:27:13 +0200

    Exchanges init file in doc tutorial. New init file has updated starting values for the parameters in line with updated values in the random init generation.

--- doc/tutorial/simulate.py
+++ doc/tutorial/simulate.py
@@ -2,7 +2,7 @@ import soepy
 from soepy.test.auxiliary import cleanup
 
 # Generate simulated dataset
-data_frame = soepy.simulate("toy_model_init_file_1000.yml")
+data_frame = soepy.simulate("toy_model_init_file_01_1000.yml")
 
 # Save data frame to csv file
 data_frame.to_pickle("test.soepy.pkl")
