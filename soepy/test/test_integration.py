281483bd929f5ff28e4aaeac34a019189c57ca1a
boryana-ilieva <boryana.a.ilieva@gmail.com>
2019-10-18 15:46:56 +0200

    Fixed integration test.

--- soepy/test/test_integration.py
+++ soepy/test/test_integration.py
@@ -6,4 +6,4 @@ def test_1():
     """This test makes sure the full package works for random initialization files."""
     random_init()
 
-    simulate("test.soepy.yml")
+    simulate("test.soepy.pkl", "test.soepy.yml")
