61f67ec0900f98e1d6e245745d803a0fc4555119
Boryana <boryana.a.ilieva@gmail.com>
2020-04-14 16:26:45 -0400

    Revised educ info format.

--- soepy/exogenous_processes/education.py
+++ soepy/exogenous_processes/education.py
@@ -1,4 +1,5 @@
-import pickle
+import pandas as pd
+
 from soepy.soepy_config import TEST_RESOURCES_DIR
 
 
@@ -7,13 +8,12 @@ def gen_prob_educ_years_vector(model_spec):
     with low, middle, and high levels of education in the SOEP data."""
 
     if "observed" in str(model_spec.educ_info_file_name).split("."):
-        with open(str(model_spec.educ_info_file_name), "rb") as f:
-            prob_educ_years = pickle.load(f)
-
+        prob_educ_years_df = pd.read_pickle(str(model_spec.educ_info_file_name))
     else:
-        with open(
-            str(TEST_RESOURCES_DIR) + "/" + str(model_spec.educ_info_file_name), "rb"
-        ) as f:
-            prob_educ_years = pickle.load(f)
+        prob_educ_years_df = pd.read_pickle(
+            str(TEST_RESOURCES_DIR) + "/" + str(model_spec.educ_info_file_name)
+        )
+
+    prob_educ_years = list(prob_educ_years_df["Fraction"])
 
     return prob_educ_years
