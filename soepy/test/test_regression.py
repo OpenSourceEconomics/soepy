da9197871de42a371c9417be2003db83f67a49d7
Boryana <boryana.a.ilieva@gmail.com>
2021-08-13 14:48:49 +0200

    Removing print statement.

--- soepy/test/test_regression.py
+++ soepy/test/test_regression.py
@@ -46,7 +46,6 @@ def test1():
         calculated_df = simulate(random_model_params_df, model_spec_init_dict)
 
         for col in expected_df.columns.tolist():
-            print(col)
             np.testing.assert_array_almost_equal(
                 expected_df[col],
                 calculated_df[col],
