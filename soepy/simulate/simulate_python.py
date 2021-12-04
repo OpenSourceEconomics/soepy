784daabac1a90f0cf50ff7bdc3a1c49bd5c25c5b
MaxBlesch <s6mables@uni-bonn.de>
2021-12-04 17:39:32 +0100

    Added function for simulation preparation.

--- soepy/simulate/simulate_python.py
+++ soepy/simulate/simulate_python.py
@@ -75,3 +75,68 @@ def simulate(model_params_init_file_name, model_spec_init_file_name, is_expected
     )
 
     return df
+
+
+def prepare_simulation_solution(model_params_init_file_name, model_spec_init_file_name,
+               is_expected=True):
+
+    # Read in model specification from yaml file
+    model_params_df, model_params = read_model_params_init(model_params_init_file_name)
+    model_spec = read_model_spec_init(model_spec_init_file_name, model_params_df)
+
+    # Get information concerning exogenous processes
+    prob_educ_level = gen_prob_educ_level_vector(model_spec)
+    prob_child_age = gen_prob_child_init_age_vector(model_spec)
+    prob_partner_present = gen_prob_partner_present_vector(model_spec)
+    prob_exp_ft = gen_prob_init_exp_vector(
+        model_spec, model_spec.ft_exp_shares_file_name
+    )
+    prob_exp_pt = gen_prob_init_exp_vector(
+        model_spec, model_spec.pt_exp_shares_file_name
+    )
+    prob_child = gen_prob_child_vector(model_spec)
+    prob_partner_arrival = gen_prob_partner_arrival(model_spec)
+    prob_partner_separation = gen_prob_partner_separation(model_spec)
+
+    # Obtain model solution
+    (
+        states,
+        indexer,
+        covariates,
+        non_employment_consumption_resources,
+        emaxs,
+        child_age_update_rule,
+        deductions_spec,
+        income_tax_spec,
+    ) = pyth_solve(
+        model_params,
+        model_spec,
+        prob_child,
+        prob_partner_arrival,
+        prob_partner_separation,
+        is_expected,
+    )
+
+    sol_dict = {
+        "model_params":model_params,
+        "model_spec":model_spec,
+        "states":states,
+        "indexer":indexer,
+        "emaxs":emaxs,
+        "covariates":covariates,
+        "non_employment_consumption_resources":non_employment_consumption_resources,
+        "deductions_spec":deductions_spec,
+        "income_tax_spec":income_tax_spec,
+        "child_age_update_rule":child_age_update_rule,
+        "prob_educ_level":prob_educ_level,
+        "prob_child_age":prob_child_age,
+        "prob_partner_present":prob_partner_present,
+        "prob_exp_ft":prob_exp_ft,
+        "prob_exp_pt":prob_exp_pt,
+        "prob_child":prob_child,
+        "prob_partner_arrival":prob_partner_arrival,
+        "prob_partner_separation":prob_partner_separation,
+        "is_expected": False,
+    }
+
+    return sol_dict
