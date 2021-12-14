bfadd007a0bd175774ae548811ed51b5a4175310
MaxBlesch <s6mables@uni-bonn.de>
2021-12-14 09:15:33 +0100

    Deactivated state space test.

--- soepy/test/test_single_woman.py
+++ soepy/test/test_single_woman.py
@@ -22,6 +22,8 @@ import pytest
 
 @pytest.fixture(scope="module")
 def input_data():
+    out = {}
+
     vault = TEST_RESOURCES_DIR / "regression_vault.soepy.pkl"
 
     with open(vault, "rb") as file:
@@ -51,72 +53,74 @@ def input_data():
         exog_partner_separation_info.to_pickle("test.soepy.partner.separation.pkl")
 
         model_params_df, model_params = read_model_params_init(random_model_params_df)
-        model_spec = read_model_spec_init(model_spec_init_dict, model_params_df)
 
-        prob_educ_level = gen_prob_educ_level_vector(model_spec)
-        prob_child_age = gen_prob_child_init_age_vector(model_spec)
-        prob_partner_present = gen_prob_partner_present_vector(model_spec)
-        prob_exp_ft = gen_prob_init_exp_vector(
-            model_spec, model_spec.ft_exp_shares_file_name
-        )
-        prob_exp_pt = gen_prob_init_exp_vector(
-            model_spec, model_spec.pt_exp_shares_file_name
-        )
-        prob_child = gen_prob_child_vector(model_spec)
-        prob_partner_arrival = gen_prob_partner_arrival(model_spec)
-        prob_partner_separation = gen_prob_partner_separation(model_spec)
-        prob_partner_arrival[:, :] = 0
-        prob_partner_present[:] = 0
+        for name, tax in [("splitted", True), ("individual", False)]:
+            model_spec_init_dict["TAXES_TRANSFERS"]["tax_splitting"] = tax
+            model_spec = read_model_spec_init(model_spec_init_dict, model_params_df)
 
-        # Solve
-        (
-            states,
-            indexer,
-            covariates,
-            non_employment_consumption_resources,
-            emaxs,
-            child_age_update_rule,
-            deductions_spec,
-            income_tax_spec,
-        ) = pyth_solve(
-            model_params,
-            model_spec,
-            prob_child,
-            prob_partner_arrival,
-            prob_partner_separation,
-            is_expected=False,
-        )
+            prob_educ_level = gen_prob_educ_level_vector(model_spec)
+            prob_child_age = gen_prob_child_init_age_vector(model_spec)
+            prob_partner_present = gen_prob_partner_present_vector(model_spec)
+            prob_exp_ft = gen_prob_init_exp_vector(
+                model_spec, model_spec.ft_exp_shares_file_name
+            )
+            prob_exp_pt = gen_prob_init_exp_vector(
+                model_spec, model_spec.pt_exp_shares_file_name
+            )
+            prob_child = gen_prob_child_vector(model_spec)
+            prob_partner_arrival = gen_prob_partner_arrival(model_spec)
+            prob_partner_separation = gen_prob_partner_separation(model_spec)
+            prob_partner_arrival[:, :] = 0
+            prob_partner_present[:] = 0
 
-        # Simulate
-        calculated_df = pyth_simulate(
-            model_params,
-            model_spec,
-            states,
-            indexer,
-            emaxs,
-            covariates,
-            non_employment_consumption_resources,
-            deductions_spec,
-            income_tax_spec,
-            child_age_update_rule,
-            prob_educ_level,
-            prob_child_age,
-            prob_partner_present,
-            prob_exp_ft,
-            prob_exp_pt,
-            prob_child,
-            prob_partner_arrival,
-            prob_partner_separation,
-            is_expected=False,
-        )
-        disc_sum = create_disc_sum_av_utility(
-            calculated_df, model_spec_init_dict["CONSTANTS"]["delta"]
-        )
-        out = {}
-        out["calculated"] = disc_sum
-        out["expected"] = -0.4464376650976864
+            # Solve
+            (
+                states,
+                indexer,
+                covariates,
+                non_employment_consumption_resources,
+                emaxs,
+                child_age_update_rule,
+                deductions_spec,
+                income_tax_spec,
+            ) = pyth_solve(
+                model_params,
+                model_spec,
+                prob_child,
+                prob_partner_arrival,
+                prob_partner_separation,
+                is_expected=False,
+            )
+
+            # Simulate
+            calculated_df = pyth_simulate(
+                model_params,
+                model_spec,
+                states,
+                indexer,
+                emaxs,
+                covariates,
+                non_employment_consumption_resources,
+                deductions_spec,
+                income_tax_spec,
+                child_age_update_rule,
+                prob_educ_level,
+                prob_child_age,
+                prob_partner_present,
+                prob_exp_ft,
+                prob_exp_pt,
+                prob_child,
+                prob_partner_arrival,
+                prob_partner_separation,
+                is_expected=False,
+            )
+
+            out[name] = create_disc_sum_av_utility(
+                calculated_df, model_spec_init_dict["CONSTANTS"]["delta"]
+            )
+
         return out
 
 
 def test_single_woman(input_data):
-    np.testing.assert_equal(input_data["calculated"], input_data["expected"])
+    np.testing.assert_equal(input_data["splitted"], input_data["individual"])
