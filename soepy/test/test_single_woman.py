9b8e460e73ec02850551356e237004f9ef053095
MaxBlesch <s6mables@uni-bonn.de>
2021-12-16 01:02:06 +0100

    Corrected regression test.

--- soepy/test/test_single_woman.py
+++ soepy/test/test_single_woman.py
@@ -117,7 +117,7 @@ def input_data():
             out[name] = create_disc_sum_av_utility(
                 calculated_df, model_spec_init_dict["CONSTANTS"]["delta"]
             )
-        out["regression_disc_sum"] = -0.5067394099836353
+        out["regression_disc_sum"] = -0.5066927046319128
         return out
 
 
