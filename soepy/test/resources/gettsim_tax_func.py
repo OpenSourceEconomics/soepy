cc15197b7ce9a724f3e1f438de0aec08895255f1
MaxBlesch <s6mables@uni-bonn.de>
2022-01-06 12:23:48 +0100

    Deleted individual rates in gettsim func.

--- soepy/test/resources/gettsim_tax_func.py
+++ soepy/test/resources/gettsim_tax_func.py
@@ -6,9 +6,7 @@ import pandas as pd
 from soepy.shared.tax_and_transfers import calculate_ssc_deductions
 
 
-def piecewise_polynomial(
-    x, thresholds, rates, intercepts_at_lower_thresholds, rates_multiplier=None
-):
+def piecewise_polynomial(x, thresholds, rates, intercepts_at_lower_thresholds):
     """Calculate value of the piecewise function at `x`.
 
     Parameters
@@ -55,44 +53,12 @@ def piecewise_polynomial(
     # Increment for each individual in the corresponding interval
     increment_to_calc = x - thresholds_individual
 
-    # Check if any value is in the lowest interval.
-    if 0 in binned.array and intercepts_at_lower_thresholds[0] == np.nan:
-        raise ValueError(f"In {x.name} is a value outside the determined range.")
+    out = binned.replace(dict(enumerate(intercepts_at_lower_thresholds)))
 
-    # If each individual has its own rates or the rates are scaled, we can't use the
-    # intercept, which was generated in the parameter loading.
-    if rates_multiplier is not None:
-
-        # Initialize Series containing 0 for all individuals
-        out = x * 0
-        out += intercepts_at_lower_thresholds[0]
-
-        # Go through all intervals except the first and last
-        for i in range(2, num_intervals):
-            threshold_incr = thresholds[i] - thresholds[i - 1]
-            for pol in range(1, degree_polynomial + 1):
-
-                # We only calculate the intercepts for individuals who are in this or
-                # higher interval. Hence we have to use the individual rates.
-                out.loc[binned >= i] += (
-                    rates_multiplier.loc[binned >= i]
-                    * rates[pol - 1, i - 1]
-                    * threshold_incr ** pol
-                )
-
-    # If rates remain the same, everything is a lot easier.
-    else:
-        # We assign each individual the pre-calculated intercept.
-        out = binned.replace(dict(enumerate(intercepts_at_lower_thresholds)))
-
-    # Intialize a multiplyer for 1 if it is not given.
-    rates_multiplier = 1 if rates_multiplier is None else rates_multiplier
     # Now add the evaluation of the increment
     for pol in range(1, degree_polynomial + 1):
-        out += (
-            binned.replace(dict(enumerate(rates[pol - 1, :])))
-            * rates_multiplier
-            * (increment_to_calc ** pol)
+        out += binned.replace(dict(enumerate(rates[pol - 1, :]))) * (
+            increment_to_calc ** pol
         )
 
     # For those in interval zero, the above equations yield wrong results
